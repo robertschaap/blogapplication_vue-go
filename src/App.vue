d5f6f003d1f2cdcc7db73d98f82b88c98d6b0300
robertschaap <robert.schaap@mac.com>
2019-04-20 21:13:45 +0200

    App: setup global styles

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,34 @@
 
 <style lang="scss">
 @import './assets/normalize.css';
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0 auto;
+  width: 80%;
+  max-width: 850px;
+  font-family: 'Open Sans', 'Helvetica Neue', 'Arial', sans-serif;
+  font-size: 0.9rem;
+}
+
+h1, h2, h3, h4, h5 {
+  font-family: 'Roboto Slab', serif;
+  margin: 0;
+}
+
+h1 {
+  font-size: 3rem;
+  color: black;
+}
+
+p {
+  margin-top: 0;
+}
 </style>
