fa3ae204eae0f011e817e99cbb7a3c63e622aad0
robertschaap <robert.schaap@mac.com>
2019-04-21 11:41:58 +0200

    PostCategoryHeader: setup component

---
+++ src/components/PostCategoryHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <section :class="$style.component">
+    <h2 v-if="label" :class="$style.header">{{ label }}</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PostCategoryHeader',
+  props: {
+    label: String,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  position: relative;
+  background-color: #eee;
+  margin-bottom: 2rem;
+  background: linear-gradient(135deg, #ffffff 0%,#cccccc 80%,#c4c4c4 67%,#c4c4c4 80%,#7db9e8 100%);
+  min-height: 150px;
+}
+
+.header {
+  font-size: 3rem;
+  padding: 1rem;
+  background-color: white;
+  font-weight: normal;
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+}
+</style>
