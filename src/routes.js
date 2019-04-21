241b2d6f07d3555fa87e89abb7473c6d4328e3b4
robertschaap <robert.schaap@mac.com>
2019-04-21 11:24:21 +0200

    routes: setup route constants

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const routes = {
+  HOME: '/',
+
+  POSTS_ALL: '/posts',
+  POSTS_NEW: '/posts/new',
+  POSTS_ONE: '/posts/:id',
+
+  USERS_LOGIN: '/users/login',
+  USERS_LOGOUT: 'users/logout',
+  USERS_NEW: '/users/new',
+};
+
+export default routes;
