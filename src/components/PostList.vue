890d80020f757875062eb2f90ce719b466639676
robertschaap <robert.schaap@mac.com>
2019-04-22 15:17:27 +0200

    PostList: setup component

---
+++ src/components/PostList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section :class="$style.component">
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  components: {},
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 1rem;
+}
+</style>
