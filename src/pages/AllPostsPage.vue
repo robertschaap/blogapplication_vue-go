f838918b30aa624566a20d81513f241d1879582a
robertschaap <robert.schaap@mac.com>
2019-04-22 15:18:45 +0200

    AllPostsPage: add placeholder posts

--- src/pages/AllPostsPage.vue
+++ src/pages/AllPostsPage.vue
@@ -1,11 +1,35 @@
 <template>
   <main>
-    Blog Vue & Go - All Posts
+    <PostCategoryHeader label="All Posts" />
+    <PostList>
+      <PostListTile
+        v-for="post in posts"
+        :key="post.id"
+        :author="post.author"
+        :title="post.title" />
+    </PostList>
   </main>
 </template>
 
 <script>
+import PostCategoryHeader from '@/components/PostCategoryHeader';
+import PostList from '@/components/PostList';
+import PostListTile from '@/components/PostListTile';
+
 export default {
   name: 'AllPostsPage',
+  components: {
+    PostCategoryHeader,
+    PostList,
+    PostListTile,
+  },
+  data: () => ({
+    posts: [
+      { id: 0, title: 'Title', author: 'Author' },
+      { id: 1, title: 'Title', author: 'Author' },
+      { id: 2, title: 'Title', author: 'Author' },
+      { id: 3, title: 'Title', author: 'Author' },
+    ],
+  }),
 };
 </script>
