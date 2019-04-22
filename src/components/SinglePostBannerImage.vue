9e88f3b6dc50bce06fb4ce051c083458006907c9
robertschaap <robert.schaap@mac.com>
2019-04-22 16:38:28 +0200

    SinglePostBannerImage: setup component

---
+++ src/components/SinglePostBannerImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <section :class="$style.component" />
+</template>
+
+<script>
+export default {
+  name: 'SinglePostBannerImage',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  position: relative;
+  min-height: 300px;
+  background-color: #eee;
+  margin-bottom: 2rem;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center top;
+}
+</style>
