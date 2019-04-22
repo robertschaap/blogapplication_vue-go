7e193a1c69a64b9419b7e30ea1b36852ac5a2ddc
robertschaap <robert.schaap@mac.com>
2019-04-22 15:16:07 +0200

    router: add routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Routes from './routes';
+
+import AllPostsPage from './pages/AllPostsPage.vue';
 import HomePage from './pages/HomePage.vue';
+import NewPostPage from './pages/NewPostPage.vue';
+import NewUserPage from './pages/NewUserPage.vue';
+import OnePostPage from './pages/OnePostPage.vue';
 
 Vue.use(Router);
 
@@ -8,10 +14,12 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'HomePage',
-      component: HomePage,
-    },
+    { path: Routes.HOME, component: HomePage },
+
+    { path: Routes.POSTS_ALL, component: AllPostsPage },
+    { path: Routes.POSTS_NEW, component: NewPostPage },
+    { path: Routes.POSTS_ONE, component: OnePostPage },
+
+    { path: Routes.USERS_NEW, component: NewUserPage },
   ],
 });
