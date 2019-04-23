31607cece578e9c5251cc529846eecd7d4e7e1be
robertschaap <robert.schaap@mac.com>
2019-04-23 15:41:48 +0200

    FormSubmitButton: setup component

---
+++ src/components/FormSubmitButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button :class="$style.component" type="submit">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'FormSubmitButton',
+  props: {
+    label: String,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  display: block;
+  border: 1px solid #666;
+  background-color: #666;
+  color: white;
+  padding: 0.5rem;
+  margin-top: 1rem;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: transparent;
+    border: 1px solid #666;
+    color: #666;
+  }
+}
+</style>
