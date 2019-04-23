769bb4e3bff0452e2e91621da91ccb9375c92fbc
robertschaap <robert.schaap@mac.com>
2019-04-23 15:42:42 +0200

    NewPost: setup component

---
+++ src/components/NewPost.vue
@@ -0,0 +1,18 @@
+<template>
+  <section :class="$style.component">
+    <h2>Create Something New</h2>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NewPost',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  padding: 0.5em;
+}
+</style>
