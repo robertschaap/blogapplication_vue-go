17687f268aacc3868453e1dfbef465b74a24734b
robertschaap <robert.schaap@mac.com>
2019-04-23 14:20:03 +0200

    SinglePostPage: add base post and comments components

--- src/pages/SinglePostPage.vue
+++ src/pages/SinglePostPage.vue
@@ -1,11 +1,38 @@
 <template>
   <main>
-    Blog Vue & Go - Single Post
+    <SinglePostBannerImage />
+    <SinglePostContent />
+    <SinglePostComments>
+      <SinglePostComment
+        v-for="(comment, index) in comments"
+        :key="index"
+        :body="comment.body"
+        :firstName="comment.firstName"
+        :lastName="comment.lastName" />
+    </SinglePostComments>
   </main>
 </template>
 
 <script>
+import SinglePostBannerImage from '@/components/SinglePostBannerImage';
+import SinglePostComment from '@/components/SinglePostComment';
+import SinglePostComments from '@/components/SinglePostComments';
+import SinglePostContent from '@/components/SinglePostContent';
+
 export default {
   name: 'SinglePostPage',
+  data: () => ({
+    comments: [{
+      body: 'Body',
+      firstName: 'First',
+      lastName: 'Last',
+    }],
+  }),
+  components: {
+    SinglePostBannerImage,
+    SinglePostComment,
+    SinglePostComments,
+    SinglePostContent,
+  },
 };
 </script>
