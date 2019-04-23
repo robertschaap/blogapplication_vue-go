9740a2fce8befec959ebf43a1864136b63837651
robertschaap <robert.schaap@mac.com>
2019-04-23 15:41:22 +0200

    FormSelect: setup base component

---
+++ src/components/FormSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <FormGroup :label="label">
+    <select :class="$style.component">
+      <option value="val">option</option>
+    </select>
+  </FormGroup>
+</template>
+
+<script>
+import FormGroup from '@/components/FormGroup';
+
+export default {
+  name: 'FormSelect',
+  props: {
+    label: String,
+  },
+  components: {
+    FormGroup,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  display: block;
+  width: 100%;
+  background-color: white;
+}
+</style>
