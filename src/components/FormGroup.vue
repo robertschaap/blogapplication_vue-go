cd83c1729ea4ef6c9098b65466085185955b956a
robertschaap <robert.schaap@mac.com>
2019-04-23 15:39:27 +0200

    FormGroup: setup component

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="$style.component">
+    <FormLabel :label="label" />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import FormLabel from '@/components/FormLabel';
+
+export default {
+  name: 'FormGroup',
+  props: {
+    label: String,
+  },
+  components: {
+    FormLabel,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  margin-top: 1rem;
+}
+</style>
