5ea66c2234d14f7da8eed750c047ed5f5f457705
robertschaap <robert.schaap@mac.com>
2019-04-23 14:18:19 +0200

    SinglePostComments: setup component

---
+++ src/components/SinglePostComments.vue
@@ -0,0 +1,19 @@
+<template>
+  <section :class="$style.component">
+    <h3>Comments</h3>
+    <p>No Comments yet, be the first!</p>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Comments',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  padding: 0.5rem;
+}
+</style>
