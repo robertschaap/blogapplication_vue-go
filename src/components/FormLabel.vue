a6d1c5d5d95587ce4b4a16c833d27a64889e8acb
robertschaap <robert.schaap@mac.com>
2019-04-23 15:38:31 +0200

    FormLabel: setup component

---
+++ src/components/FormLabel.vue
@@ -0,0 +1,19 @@
+<template>
+  <label :class="$style.component">{{ label }}</label>
+</template>
+
+<script>
+export default {
+  name: 'FormLabel',
+  props: {
+    label: String,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  display: block;
+  width: 100%;
+}
+</style>
