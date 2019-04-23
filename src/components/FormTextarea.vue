2b553af3491166919c5528a40b2aeff6b8c7f510
robertschaap <robert.schaap@mac.com>
2019-04-23 15:40:26 +0200

    FormTextarea: setup component

---
+++ src/components/FormTextarea.vue
@@ -0,0 +1,27 @@
+<template>
+  <FormGroup :label="label">
+    <textarea :class="$style.component"/>
+  </FormGroup>
+</template>
+
+<script>
+import FormGroup from '@/components/FormGroup';
+
+export default {
+  name: 'FormTextarea',
+  props: {
+    label: String,
+  },
+  components: {
+    FormGroup,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  display: block;
+  width: 100%;
+  border: 1px solid #eee;
+}
+</style>
