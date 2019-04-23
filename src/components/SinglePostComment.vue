b13c48d1f0f44d5f8d9a6057d37f83a58e5c213a
robertschaap <robert.schaap@mac.com>
2019-04-23 14:17:58 +0200

    SinglePostComment: setup component

---
+++ src/components/SinglePostComment.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="$style.component">
+    <div :class="$style.image" />
+    <div :class="$style.body">
+      <p :class="$style.author">{{ firstName }} {{ lastName }}</p>
+      <p>{{ body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SinglePostComment',
+  props: {
+    body: String,
+    firstName: String,
+    lastName: String,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  display: flex;
+}
+
+.image {
+  flex: none;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #eee;
+  margin-right: 1rem;
+}
+
+.body {
+  flex: auto;
+}
+
+.author {
+  margin-bottom: 0;
+  border-bottom: 1px solid #eee;
+}
+</style>
