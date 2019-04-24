4e45af0dd048193c3eb38751c8fe15b4c3a4fc95
robertschaap <robert.schaap@mac.com>
2019-04-24 13:39:42 +0200

    NewUser: setup component

---
+++ src/components/NewUser.vue
@@ -0,0 +1,18 @@
+<template>
+  <section :class="$style.component">
+    <h2>Join Us!</h2>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NewUser',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  padding: 0.5em;
+}
+</style>
