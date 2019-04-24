c25fe005bb04959ec9c97b9e6e3faedb5f31e410
robertschaap <robert.schaap@mac.com>
2019-04-24 13:35:21 +0200

    FormCheckbox: setup component

---
+++ src/components/FormCheckbox.vue
@@ -0,0 +1,37 @@
+<template>
+  <FormGroup :label="label">
+    <input
+      :type="type"
+      :checked="checked"
+      @change="$emit('input', $event.target.checked)"
+      :class="$style.component" />
+  </FormGroup>
+</template>
+
+<script>
+import FormGroup from '@/components/FormGroup';
+
+export default {
+  name: 'FormCheckbox',
+  props: {
+    label: String,
+    checked: Boolean,
+    type: {
+      type: String,
+      default: 'checkbox',
+      validator: prop => prop === 'checkbox',
+    },
+  },
+  components: {
+    FormGroup,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  display: block;
+  width: 100%;
+  border: 1px solid #eee;
+}
+</style>
