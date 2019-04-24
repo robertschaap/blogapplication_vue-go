1df508c04b5571ccd95ec85848b9843f9cb4663b
robertschaap <robert.schaap@mac.com>
2019-04-24 13:45:35 +0200

    NewUserForm: setup component

---
+++ src/components/NewUserForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <form :class="$style.component">
+    <FormInput
+      v-model="form.firstName"
+      label="First Name" />
+    <FormInput
+      v-model="form.lastName"
+      label="Last Name" />
+    <FormInput
+      v-model="form.email"
+      label="E-mail" />
+    <FormInput
+      v-model="form.userName"
+      label="User Name" />
+    <FormTextarea
+      v-model="form.bio"
+      label="Bio" />
+    <FormInput
+      v-model="form.password"
+      label="Password (Minimum 8 Characters)" />
+    <FormInput
+      v-model="form.passwordConfirm"
+      label="Confirm Password" />
+
+    <FormSubmitButton
+      label="Submit" />
+  </form>
+</template>
+
+<script>
+import FormInput from '@/components/FormInput';
+import FormSubmitButton from '@/components/FormSubmitButton';
+import FormTextarea from '@/components/FormTextarea';
+
+export default {
+  name: 'NewUserForm',
+  data: () => ({
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      userName: '',
+      bio: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  }),
+  components: {
+    FormInput,
+    FormSubmitButton,
+    FormTextarea,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  width: 50%;
+}
+</style>
