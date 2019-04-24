bbea597dbea8c251c17515318c86178e8b697c71
robertschaap <robert.schaap@mac.com>
2019-04-24 12:42:40 +0200

    Login: setup component

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <section :class="$style.component">
+    <h2>Sign In</h2>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+  padding: 0.5em;
+}
+</style>
