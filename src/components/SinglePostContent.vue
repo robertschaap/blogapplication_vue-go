3b6cc213ba86b5feebe09822353c18f0cea92e91
robertschaap <robert.schaap@mac.com>
2019-05-04 23:19:07 +0200

    SinglePostContent: added avatar and styling

--- src/components/SinglePostContent.vue
+++ src/components/SinglePostContent.vue
@@ -6,7 +6,9 @@
         <p>Robert schaap</p>
         <p>Robert is aa true hero. He taught himself to code with the help of courses, books and a bit of magic.</p>
       </div>
-      <div :class="$style.headerAvatar" />
+      <div :class="$style.headerAvatar">
+        <img :class="$style.headerAvatarImage" src="/avatars/1.jpg" />
+      </div>
     </div>
 
     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
@@ -39,6 +41,12 @@ export default {
     border-radius: 50%;
     background-color: #eee;
     margin-left: 1rem;
+    overflow: hidden;
+
+    &Image {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 
