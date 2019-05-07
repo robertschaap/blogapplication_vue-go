fdb34403ab887b627fa2e58e650ac22b2ca5fd1e
robertschaap <robert.schaap@mac.com>
2019-05-07 23:11:50 +0200

    Categories: setup enum file

---
+++ src/Categories.js
@@ -0,0 +1,9 @@
+const Categories = {
+  all: 'All Posts',
+  technology: 'Technology',
+  design: 'Design',
+  entrepreneur: 'Entrepreneur',
+  anything: 'Anything',
+};
+
+export default Categories;
