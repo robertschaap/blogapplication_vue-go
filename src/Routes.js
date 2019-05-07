87180f00ea09185013972b6ba8da027ad81ef6fd
robertschaap <robert.schaap@mac.com>
2019-05-07 22:59:28 +0200

    Routes: add regex to posts routes to match cat or id properly

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,9 @@
 const Routes = {
   HOME: '/',
 
-  POSTS_ALL: '/posts',
+  POSTS_ALL: '/posts/:category(\\D+)',
   POSTS_NEW: '/posts/new',
-  POSTS_SINGLE: '/posts/:id',
+  POSTS_SINGLE: '/posts/:id([0-9a-fA-F]{24})',
 
   USERS_LOGIN: '/users/login',
   USERS_LOGOUT: 'users/logout',
