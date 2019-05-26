dff67f22b84ce319d2e5fbdff1ba02d6ed754c2e
robertschaap <robert.schaap@mac.com>
2019-05-26 12:17:17 +0200

    posts controller: add getPostsHandler

--- server/controllers/posts.go
+++ server/controllers/posts.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+)
+
+// GetPostsHandler doesnt do much yet
+func GetPostsHandler(w http.ResponseWriter, r *http.Request) {
+	type Post struct {
+		ID      int			`json:"id"`
+		Title   string	`json:"title"`
+		Author  string	`json:"author"`
+	}
+
+	var post []Post
+
+	post = append(post, Post{ 0, "title", "author" })
+	post = append(post, Post{ 1, "title", "author" })
+	post = append(post, Post{ 2, "title", "author" })
+	post = append(post, Post{ 3, "title", "author" })
+	post = append(post, Post{ 4, "title", "author" })
+	post = append(post, Post{ 5, "title", "author" })
+
+	fmt.Println(post)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(post)
+}
