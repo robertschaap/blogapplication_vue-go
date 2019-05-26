e55064df3e3d8921de00caf387ed553903cd1639
robertschaap <robert.schaap@mac.com>
2019-05-26 14:36:08 +0200

    main.go: import controllers as c

--- server/main.go
+++ server/main.go
@@ -6,19 +6,19 @@ import (
 	"log"
 	"net/http"
 
-	"./controllers"
+	c "./controllers"
 )
 
 func startServer() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/comments/new", controllers.CreateCommentHandler).Methods("POST")
-	r.HandleFunc("/api/posts", controllers.GetPostsHandler).Methods("GET")
-	r.HandleFunc("/api/posts/{category}", controllers.GetPostsByCategoryHandler).Methods("GET")
-	r.HandleFunc("/api/post/{id}", controllers.GetPostHandler).Methods("GET")
-	r.HandleFunc("/api/post/new", controllers.CreatePostHandler).Methods("POST")
-	r.HandleFunc("/api/users/new", controllers.CreateUserHandler).Methods("POST")
-	r.HandleFunc("/api/users/login", controllers.GetUserLoginHandler).Methods("GET")
+	r.HandleFunc("/api/comments/new", c.CreateCommentHandler).Methods("POST")
+	r.HandleFunc("/api/posts", c.GetPostsHandler).Methods("GET")
+	r.HandleFunc("/api/posts/{category}", c.GetPostsByCategoryHandler).Methods("GET")
+	r.HandleFunc("/api/post/{id}", c.GetPostHandler).Methods("GET")
+	r.HandleFunc("/api/post/new", c.CreatePostHandler).Methods("POST")
+	r.HandleFunc("/api/users/new", c.CreateUserHandler).Methods("POST")
+	r.HandleFunc("/api/users/login", c.GetUserLoginHandler).Methods("GET")
 
 	port := ":3001"
 
