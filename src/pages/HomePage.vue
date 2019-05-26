7b66c68533d2025b22566b242384ceb1d7a4f060
robertschaap <robert.schaap@mac.com>
2019-05-26 12:19:46 +0200

    HomePage: add fetchPosts call

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -23,16 +23,7 @@ export default {
     PostListTile,
   },
   data: () => ({
-    posts: [
-      { id: 0, title: 'Title', author: 'Author' },
-      { id: 1, title: 'Title', author: 'Author' },
-      { id: 2, title: 'Title', author: 'Author' },
-      { id: 3, title: 'Title', author: 'Author' },
-      { id: 4, title: 'Title', author: 'Author' },
-      { id: 5, title: 'Title', author: 'Author' },
-      { id: 6, title: 'Title', author: 'Author' },
-      { id: 7, title: 'Title', author: 'Author' },
-    ],
+    posts: [],
   }),
   computed: {
     homePosts: function () {
@@ -41,6 +32,17 @@ export default {
         isLarge: index < 2,
         isMedium: index >= 2 && index < 4,
       }));
+    },
+  },
+  created () {
+    this.fetchPosts();
+  },
+  methods: {
+    async fetchPosts () {
+      const res = await fetch('/api/posts');
+      const json = await res.json();
+
+      this.posts = json;
     },
   },
 };
