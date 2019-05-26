51932b43aa768c8876ab12174449440dce8defdc
robertschaap <robert.schaap@mac.com>
2019-05-26 14:12:18 +0200

    posts controller: setup base GetPostsByCategoryHandler

--- server/controllers/posts.go
+++ server/controllers/posts.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/gorilla/mux"
 	"fmt"
 	"encoding/json"
 	"net/http"
@@ -24,6 +25,30 @@ func GetPostsHandler(w http.ResponseWriter, r *http.Request) {
 	post = append(post, Post{ 5, "title", "author" })
 
 	fmt.Println(post)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(post)
+}
+
+// GetPostsByCategoryHandler doesnt do much yet
+func GetPostsByCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	category := mux.Vars(r)["category"]
+	fmt.Println(category)
+
+	type Post struct {
+		ID      int			`json:"id"`
+		Title   string	`json:"title"`
+		Author  string	`json:"author"`
+	}
+
+	var post []Post
+
+	post = append(post, Post{ 0, "title", "author" })
+	post = append(post, Post{ 1, "title", "author" })
+	post = append(post, Post{ 2, "title", "author" })
+	post = append(post, Post{ 3, "title", "author" })
+	post = append(post, Post{ 4, "title", "author" })
+	post = append(post, Post{ 5, "title", "author" })
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(post)
