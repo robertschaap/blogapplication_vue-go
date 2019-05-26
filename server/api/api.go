b3fba7817326467e192d291552602f1ecb546d3d
robertschaap <robert.schaap@mac.com>
2019-05-26 17:07:17 +0200

    server: add JSON method to Response

--- server/api/api.go
+++ server/api/api.go
@@ -1,8 +1,19 @@
 package api
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // Response is an Response
 type Response struct {
 	Status	 string 		  `json:"status"`
 	Data		 interface{}  `json:"data"`
 	Message	 string 			`json:"message"`
+}
+
+// JSON is a Json
+func (r *Response) JSON(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(r)
 }
