ec79591fe95017f226c00215abbe3b307b106d76
robertschaap <robert.schaap@mac.com>
2019-05-28 20:39:02 +0200

    Routes: re-order post routes for regex matching

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,10 @@
 const Routes = {
   HOME: '/',
 
+  // Order is specific for regex matching
+  POSTS_SINGLE: '/posts/:id([0-9])',
   POSTS_ALL: '/posts/:category(\\D+)',
   POSTS_NEW: '/posts/new',
-  POSTS_SINGLE: '/posts/:id([0-9a-fA-F]{24})',
 
   USERS_LOGIN: '/users/login',
   USERS_LOGOUT: 'users/logout',
