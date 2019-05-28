1b4042d95ea54f55a2c7b3f621e3f9803474a75d
robertschaap <robert.schaap@mac.com>
2019-05-28 19:18:03 +0200

    api.go: add proper comments to exports

--- server/api/api.go
+++ server/api/api.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-// Response is an Response
+// Response struct provides a standardised wrapper for api responses
 type Response struct {
 	Status	 string 		  `json:"status"`
 	Data		 interface{}  `json:"data"`
 	Message	 string 			`json:"message"`
 }
 
-// JSON is a Json
+// JSON method sets json content type and encodes the response
 func (r *Response) JSON(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(r)
