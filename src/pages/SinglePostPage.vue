e0f48e4858f20044f7ffbedac28edcc0342c1e7e
robertschaap <robert.schaap@mac.com>
2019-05-30 22:57:23 +0200

    SinglePostPage: add api call

--- src/pages/SinglePostPage.vue
+++ src/pages/SinglePostPage.vue
@@ -1,9 +1,9 @@
 <template>
-  <main>
+  <main v-if="body">
     <SinglePostBannerImage />
     <SinglePostContent
-      :author="post.author"
-      :body="post.body" />
+      :author="author"
+      :body="body" />
     <SinglePostComments>
       <SinglePostComment
         v-for="(comment, index) in comments"
@@ -21,36 +21,37 @@ import SinglePostComment from '@/components/SinglePostComment';
 import SinglePostComments from '@/components/SinglePostComments';
 import SinglePostContent from '@/components/SinglePostContent';
 
+import ApiStatusType from '@/ApiStatusType';
+
 export default {
   name: 'SinglePostPage',
   data: () => ({
-    comments: [{
-      body: 'Body',
-      firstName: 'First',
-      lastName: 'Last',
-    }],
-    post: {
-      author: {
-        bio: 'Robert is a true hero. He taught himself to code with the help of courses, books and a bit of magic.',
-        firstName: 'Robert',
-        lastName: 'Schaap',
-      },
-      body: {
-        body: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
-        title: 'That feeling when you\'re coding in a new framework',
-      },
-      comments: [{
-        body: 'Body',
-        firstName: 'First',
-        lastName: 'Last',
-      }],
-    },
+    author: null,
+    body: null,
+    comments: null,
   }),
   components: {
     SinglePostBannerImage,
     SinglePostComment,
     SinglePostComments,
     SinglePostContent,
+  },
+  created () {
+    this.fetchPost();
+  },
+  methods: {
+    async fetchPost () {
+      const { id } = this.$route.params;
+
+      const res = await fetch(`/api/post/${id}`);
+      const json = await res.json();
+
+      if (json.status === ApiStatusType.SUCCESS) {
+        this.body = json.data.body;
+        this.author = json.data.author;
+        this.comments = json.data.comments;
+      }
+    },
   },
 };
 </script>
