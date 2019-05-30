a2bc7c51e0a1bc6543150584c7ff907b4b8cb980
robertschaap <robert.schaap@mac.com>
2019-05-30 22:08:06 +0200

    SinglePostPage: pass post body and author to SinglePostContent

--- src/pages/SinglePostPage.vue
+++ src/pages/SinglePostPage.vue
@@ -1,7 +1,9 @@
 <template>
   <main>
     <SinglePostBannerImage />
-    <SinglePostContent />
+    <SinglePostContent
+      :author="post.author"
+      :body="post.body" />
     <SinglePostComments>
       <SinglePostComment
         v-for="(comment, index) in comments"
@@ -27,6 +29,17 @@ export default {
       firstName: 'First',
       lastName: 'Last',
     }],
+    post: {
+      author: {
+        bio: 'Robert is a true hero. He taught himself to code with the help of courses, books and a bit of magic.',
+        firstName: 'Robert',
+        lastName: 'Schaap',
+      },
+      body: {
+        body: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
+        title: 'That feeling when you\'re coding in a new framework',
+      },
+    },
   }),
   components: {
     SinglePostBannerImage,
