1c4d2777832277f5fb2be2c043094dc6343dce2e
robertschaap <robert.schaap@mac.com>
2019-06-03 20:08:55 +0200

    LoginPage: add login handling

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <Login>
-      <LoginForm />
+      <LoginForm :handleSubmit="handleSubmit" />
     </Login>
   </main>
 </template>
@@ -10,11 +10,31 @@
 import Login from '@/components/Login';
 import LoginForm from '@/components/LoginForm';
 
+import ApiStatusType from '@/ApiStatusType';
+
 export default {
   name: 'LoginPage',
   components: {
     Login,
     LoginForm,
+  },
+  methods: {
+    async handleSubmit (form) {
+      const options = {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      };
+
+      const res = await fetch(`/api/users/login`, options);
+      const json = await res.json();
+
+      if (json.status === ApiStatusType.SUCCESS) {
+        // handle login
+      }
+    },
   },
 };
 </script>
