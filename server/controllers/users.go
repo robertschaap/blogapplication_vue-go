3939f5021b56bd0cbc88c79db352c2c91e3459f9
robertschaap <robert.schaap@mac.com>
2019-06-03 20:07:00 +0200

    server: stub login method in user controller

--- server/controllers/users.go
+++ server/controllers/users.go
@@ -3,12 +3,33 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+
+	"../api"
 )
 
 // GetUserLoginHandler does stuff
 func GetUserLoginHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode("Hello World")
+	type User struct {
+		ID				string	`json:"id"`
+		FirstName	string	`json:"firstName"`
+		LastName	string	`json:"lastName"`
+		Token			string	`json:"authToken"`
+	}
+
+	user := User{
+		ID: "0",
+		FirstName: "robert",
+		LastName: "schaap",
+		Token: "mytoken",
+	}
+
+	res := api.Response{
+		Status: "success",
+		Data: user,
+		Message: "",
+	}
+
+	res.JSON(w)
 }
 
 
