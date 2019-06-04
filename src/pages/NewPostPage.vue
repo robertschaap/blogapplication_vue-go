14559ae449db96bb18dde846ee5d445bffa9ffaf
robertschaap <robert.schaap@mac.com>
2019-06-04 21:47:57 +0200

    NewPostPage: add handler for form submisison

--- src/pages/NewPostPage.vue
+++ src/pages/NewPostPage.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <NewPost>
-      <NewPostForm />
+      <NewPostForm :handleSubmit="handleSubmit" />
     </NewPost>
   </main>
 </template>
@@ -10,11 +10,31 @@
 import NewPost from '@/components/NewPost';
 import NewPostForm from '@/components/NewPostForm';
 
+import ApiStatusType from '@/ApiStatusType';
+
 export default {
   name: 'NewPostPage',
   components: {
     NewPost,
     NewPostForm,
+  },
+  methods: {
+    async handleSubmit (form) {
+      const options = {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      };
+
+      const res = await fetch(`api/post/new`, options);
+      const json = await res.json();
+
+      if (json.status === ApiStatusType.SUCCESS) {
+        // handle post
+      }
+    },
   },
 };
 </script>
