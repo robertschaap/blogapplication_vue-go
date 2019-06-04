687f4e60a3d642a40d49309c4d0961fb2523b100
robertschaap <robert.schaap@mac.com>
2019-06-04 21:48:15 +0200

    NewUserPage: add handler for form submisison

--- src/pages/NewUserPage.vue
+++ src/pages/NewUserPage.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <NewUser>
-      <NewUserForm />
+      <NewUserForm :handleSubmit="handleSubmit" />
     </NewUser>
   </main>
 </template>
@@ -10,11 +10,31 @@
 import NewUser from '@/components/NewUser';
 import NewUserForm from '@/components/NewUserForm';
 
+import ApiStatusType from '@/ApiStatusType';
+
 export default {
   name: 'NewUserPage',
   components: {
     NewUser,
     NewUserForm,
+  },
+  methods: {
+    async handleSubmit (form) {
+      const options = {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      };
+
+      const res = await fetch(`/api/users/new`, options);
+      const json = await res.json();
+
+      if (json.status === ApiStatusType.SUCCESS) {
+        // handle post
+      }
+    },
   },
 };
 </script>
